4842ae4b727d7bcf0959451b5819eae45f5b2db3
Barbeman <nickyakovlev03121990@gmail.com>
2018-09-07 17:38:37 +0300

    upd

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет мир!");
+            Console.WriteLine("Привет мир!!!!");
             Console.ReadKey();
         }
     }
